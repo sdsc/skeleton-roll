667daf8d198387dd29e7e12f35fe263dfd0e6c7b
Jim Hayes <jhayes@sdsc.edu>
2014-11-17 10:14:16 -0800

    Add sdsc- prefix.  Add Autoreq:No.

--- src/skeleton/version.mk
+++ src/skeleton/version.mk
@@ -1,15 +1,16 @@
-PKGROOT            = /opt/skeleton
-NAME               = skeleton
-VERSION            = 1.0
-RELEASE            = 1
-TARBALL_POSTFIX    = tgz
+NAME           = sdsc-skeleton
+VERSION        = 1.0
+RELEASE        = 1
+PKGROOT        = /opt/skeleton
 
-SRC_SUBDIR         = skeleton
+SRC_SUBDIR     = skeleton
 
-SOURCE_NAME        = $(NAME)
-SOURCE_VERSION     = $(VERSION)
-SOURCE_SUFFIX      = tgz
-SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
-SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+SOURCE_NAME    = skeleton
+SOURCE_SUFFIX  = tgz
+SOURCE_VERSION = $(VERSION)
+SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
-TGZ_PKGS           = $(SOURCE_PKG)
+TGZ_PKGS       = $(SOURCE_PKG)
+
+RPM.EXTRAS     = AutoReq:No
